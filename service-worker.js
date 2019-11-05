2a2046221b92c48c389577fb7622dda3c07cb3b1
ncehk2019 <51715054+ncehk2019@users.noreply.github.com>
2019-11-05 20:41:14 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nce-live/precache-manifest.0d471096b00bbcce79f63c5f46c051dd.js"
+  "/nce-live/precache-manifest.3a780b12fddc776f92f4eefc5980dd8b.js"
 );
 
 self.addEventListener('message', (event) => {
